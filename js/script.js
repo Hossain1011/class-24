995fd8c74b4193697d82719a76be4285fc93dd5c
Hossain <ahossain1011@gmail.com>
2021-08-21 01:50:18 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('#banner').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // arrows:false,
+        // autoplay:true,
+        // autoplaySpeed: 2000,
+        prevArrow: '<i class="fa fa-arrow-left left"></i>',
+        nextArrow: '<i class="fa fa-arrow-right right"></i>',
+        fade: true,
+        
+
+    });
+});
